0eeb7e87138e984027991f4ad2f697a718a06e27
dsolovew <dsolovew@gmail.com>
2021-02-09 19:51:04 +0300

    step2

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "server_group" {
   } 
 }
 
-resource "aws_instance" "server" {
+resource "aws_instance" "server1" {
   ami           = "ami-0a91cd140a1fc148a"
   instance_type = "t3.micro"
   key_name      = "ssh-key"
@@ -36,7 +36,7 @@ resource "aws_instance" "server" {
   }
 }
 
-resource "aws_instance" "server" {
+resource "aws_instance" "server2" {
   ami           = "ami-0a91cd140a1fc148a"
   instance_type = "t3.micro"
   key_name      = "ssh-key"
@@ -47,13 +47,13 @@ resource "aws_instance" "server" {
   }
 }
 
-resource "aws_instance" "server" {
+resource "aws_instance" "server3" {
   ami           = "ami-0a91cd140a1fc148a"
   instance_type = "t3.micro"
   key_name      = "ssh-key"
   vpc_security_group_ids = [aws_security_group.server_group.id]
   tags = {
-    "Name"  = "Server3ste"
+    "Name"  = "Server3"
     "Owner" = "Denis Solovyev"
   }
 }
